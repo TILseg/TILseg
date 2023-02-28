ca924fcdac87a6e9eb5daa141cd437a6d1f79b55
Braden-Griebel <bgriebel@uw.edu>
2023-02-28 12:51:47 -0800

    Created practive unittest

---
+++ tilseg/test/test_ci.py
@@ -0,0 +1,14 @@
+"""
+Tests for continous integration
+"""
+import unittest
+
+class TestContinousIntegration(unittest.TestCase):
+    """
+    Class for testing continous integration
+    """
+    def test_continous_integration(self):
+        """
+        Function to test continous integration
+        """
+        self.assertEqual(2,2)
