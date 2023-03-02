8083b3b3521802542452cd6f225671b8173477b3
Ryan Brady <bradyr18@uw.edu>
2023-03-02 11:47:02 -0800

    Made some subfunctions and reformated based on pylint

--- ClusterProcessing.py
+++ ClusterProcessing.py
@@ -4,44 +4,29 @@ analysis based on the clusters generated via the clustering model previously
 developed. The intended output is a series of images which represent the
 original image and relevant overlays of the determined clusters. Additionally,
 based on the clusters, data from filtered cell clusters will be compiled into
-a CSV. 
+a CSV.
 """
+import os
 
 import numpy as np
 import  cv2 as cv
-import os 
 #from PIL import Image
 
-def ClusterSplit(img_clust: np.ndarray, clust_count: int):
-    """
-    This function is designed to take an input clustered image and turn it into
-    a series of masks which can be further processed by OpenCV to identify cell
-    groups
-
-    Inputs:
-    -img_clust: a 3D array where two dimensions represent the x and y coordinates
-        of the image and the third dimension represents RGB and asigned cluster
-    -clust_count: an integer 
-
-    Output:
-    -Dictionary of binary image arrays for each cluster
-    """
-
-def ContourGenerator(img_mask: np.ndarray):
+def contour_generator(img_mask: np.ndarray):
     """
     Creates contours based on an inputted mask and parameters defined herein.
-    These parameters define what will be classified as likely an immune 
+    These parameters define what will be classified as likely an immune
     cell cluster and can be varied within this code block.
-    
+
     Input:
     -img_mask: binary 2D array where the dimensions represent the x and y
         coordinates of the relevant pixels
-    
-    Output: 
+
+    Output:
     -Contour object?
     """
 
-def DataSummaryGenerator(cont_dict: dict, filepath: str):
+def data_summary_generator(cont_dict: dict, filepath: str):
     """
     Generates CSV file with relevant areas, intensities, and circularities
     of previously identified cell groups
@@ -60,16 +45,44 @@ for i in range(3000):
         test_array[i][j][3]=cluster[i][j]
 np.save("/home/bradyr18/both.npy", test_array)
 '''
-original_image = np.load("/home/bradyr18/patch.npy")
-test_array = np.load("/home/bradyr18/both.npy")
 
-def ImageOverlayGenerator(img_clust: np.ndarray, original_image: np.ndarray, clust_count: int, filepath: str):
+def gen_base_arrays(ori_image: np.ndarray, num_clust: int, array_dims: list):
+    """
+    Generates set of two arrays which will be overlaid with the cluster data.
+    The first array contains the number of clusters+1 images which will recieve
+    masks based on the associated cluster. The second is a binary array for use
+    in contour generation.
+    """
+    four_dim_array=np.expand_dims(ori_image, 0)
+    binary_array=np.full((num_clust, array_dims[0], array_dims[1]),False)
+    final_array=four_dim_array
+    for _ in range(num_clust):
+        final_array=np.vstack((final_array, four_dim_array))
+    return final_array, binary_array
+
+def generate_images(image_array: np.ndarray, filepath: str, num_clust: int):
+    """
+    This takes in an array of image values and generates a directory of
+    jpg images in a specified file location.
+    """
+    path=os.path.join(filepath, "Overlaid Images")
+    os.mkdir(path)
+    os.chdir(path)
+    for i in range(num_clust+1):
+        if i !=0:
+            cv.imwrite(f"Image{i}.jpg", image_array[i][:][:][:])
+        else:
+            cv.imwrite("Original.jpg", image_array[i][:][:][:])
+    return None
+
+def image_overlay_generator(img_clust: np.ndarray, original_image: np.ndarray,
+                             clust_count: int, filepath: str):
     """
     Generates series of images equal to the number of clusters plus the
     original and saves it to the specified filepath
     """
     #Colors that will become associated with each cluster on overlays
-    OverlayColor=np.array(
+    overlay_color=np.array(
         [[0, 0, 0],
         [0, 0, 0],
         [0, 0, 0],
@@ -81,23 +94,22 @@ def ImageOverlayGenerator(img_clust: np.ndarray, original_image: np.ndarray, clu
 
     #Making a dictionary of the original images that will be overwriten
     dims = img_clust.shape
-    FourDImage=np.expand_dims(original_image, 0)
-    FinalArrays=FourDImage
-    for i in range(clust_count):
-        FinalArrays=np.vstack((FinalArrays, FourDImage))
+
+    final_arrays, binary_arrays = gen_base_arrays(original_image, clust_count, dims)
 
     for j in range(dims[0]):
         for k in range(dims[1]):
             key=int(img_clust[j][k][3])
-            FinalArrays[key+1][j][k]=OverlayColor[key]
+            final_arrays[key][j][k]=overlay_color[key]
+            binary_arrays[key][j][k]=True
 
-    path=os.path.join(filepath, "ImageStack1")
-    os.mkdir(path)
-    os.chdir(path)
-    for i in range(clust_count+1):
-        cv.imwrite(f"Image{i}.jpg", FinalArrays[i][:][:][:])
-    return FinalArrays
+    generate_images(final_arrays, filepath, clust_count)
+
+    return final_arrays, binary_arrays
 
 
-final_arrays=ImageOverlayGenerator(test_array, original_image, 8, "/home/bradyr18")
 
+original_image1 = np.load("/home/bradyr18/patch.npy")
+test_array = np.load("/home/bradyr18/both.npy")
+
+final, binary=image_overlay_generator(test_array, original_image1, 8, "/home/bradyr18")
