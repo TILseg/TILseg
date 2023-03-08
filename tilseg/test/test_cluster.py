b238f358b0dc5f6dddf0f0cfa3bd37de347400a4
Abishek Sankaranarayanan <abishek.sankara@gmail.com>
2023-03-08 01:03:04 -0800

    integrated Ryan's code inot cluster but getting errors

--- tilseg/test/test_cluster.py
+++ tilseg/test/test_cluster.py
@@ -6,6 +6,10 @@ import unittest
 import tilseg.cluster
 import sklearn.linear_model
 import numpy as np
+import sklearn.cluster
+
+from PIL import UnidentifiedImageError
+from sklearn.exceptions import NotFittedError
 
 test_patch_path = '/Users/abishek/Desktop/DataScienceClasses/TILSeg/abi_patches/test_patch.tif'
 fail_test_patch_path = '/Users/abishek/Desktop/DataScienceClasses/TILSeg/abi_patches/test_img.txt'
@@ -32,102 +36,80 @@ class TestClusterModelFitter(unittest.TestCase):
         """
         Tests that non-string input for patch_path is dealt with
         """
-        try:
+        with self.assertRaises(TypeError):
             model = tilseg.cluster.cluster_model_fitter(
                 patch_path=2,
                 algorithm='KMeans',
                 n_clusters=4)
-            self.assertTrue(False)
-        except TypeError:
-            self.assertTrue(True)
 
     def test_patch_file(self):
         """
         Testing when input file does not exist
         """
-        try:
+        with self.assertRaises(ValueError):
             model = tilseg.cluster.cluster_model_fitter(
                 patch_path=test_patch_path+'blahblah',
                 algorithm='KMeans',
                 n_clusters=4)
-            self.assertTrue(False)
-        except ValueError:
-            self.assertTrue(True)
  
-    # def test_patch_img(self):
-    #     """
-    #     Testing when input file is not an image
-    #     """
-    #     class UnidentifiedImageError(Exception):
-    #         pass
-    #     try:
-    #         model = tilseg.cluster.cluster_model_fitter(
-    #             patch_path=fail_test_patch_path,
-    #             algorithm='KMeans',
-    #             n_clusters=4)
-    #         self.assertTrue(False)
-    #     except UnidentifiedImageError:
-    #         self.assertTrue(True)
+    def test_patch_img(self):
+        """
+        Testing when input file is not an image
+        """
+        with self.assertRaises(UnidentifiedImageError):
+            model = tilseg.cluster.cluster_model_fitter(
+                patch_path=fail_test_patch_path,
+                algorithm='KMeans',
+                n_clusters=4)
 
     def test_algorithm_in(self):
         """
         Testing when non-string input for algorithm
         """
-        try:
+        with self.assertRaises(ValueError):
             model = tilseg.cluster.cluster_model_fitter(
                 patch_path=test_patch_path,
                 algorithm=5,
                 n_clusters=4)
-            self.assertTrue(False)
-        except ValueError:
-            self.assertTrue(True)
 
     def test_n_clusters_KMeans(self):
         """
         Testing that n_clusters is numerical when algorithm is KMeans
         """
-        try:
+        with self.assertRaises(ValueError):
             model = tilseg.cluster.cluster_model_fitter(
                 patch_path=test_patch_path,
                 algorithm='KMeans'
                 )
-            self.assertTrue(False)
-        except ValueError:
-            self.assertTrue(True)
 
     def test_n_clusters_in1(self):
         """
         Tests when n_cluster input is not an integer
         """
-        try:
+        with self.assertRaises(ValueError):
             model = tilseg.cluster.cluster_model_fitter(
                 patch_path=test_patch_path,
                 algorithm='KMeans',
                 n_clusters='4'
                 )
-            self.assertTrue(False)
-        except ValueError:
-            self.assertTrue(True)
 
     def test_n_clusters_in2(self):
         """
         Tests when n_cluster input is more than 8
         """
-        try:
+        with self.assertRaises(ValueError):
             model = tilseg.cluster.cluster_model_fitter(
                 patch_path=test_patch_path,
                 algorithm='KMeans',
                 n_clusters=9
                 )
-            self.assertTrue(False)
-        except ValueError:
-            self.assertTrue(True)
-
 
 model_KMeans = tilseg.cluster.cluster_model_fitter(
                 patch_path=test_patch_path,
                 algorithm='KMeans',
                 n_clusters=4)
+
+model_unfitted = model = sklearn.cluster.KMeans(n_clusters=4, max_iter=20, n_init=3, tol=1e-3)
 
 model_linear = sklearn.linear_model.LinearRegression()
 model_linear.fit(np.random.rand(5).reshape(-1,1), np.random.rand(5))
@@ -150,60 +132,40 @@ class TestClusteringScore(unittest.TestCase):
         """
         Tests that non-string input for patch_path is dealt with
         """
-        try:
+        with self.assertRaises(TypeError):
             [ch_score, db_score] = tilseg.cluster.clustering_score(model=model_KMeans, patch_path=2)
-            self.assertTrue(False)
-        except TypeError:
-            self.assertTrue(True)
 
     def test_patch_file(self):
         """
         Testing when input file does not exist
         """
-        try:
+        with self.assertRaises(ValueError):
             [ch_score, db_score] = tilseg.cluster.clustering_score(model=model_KMeans, patch_path=test_patch_path+'blahblah')
-            self.assertTrue(False)
-        except ValueError:
-            self.assertTrue(True)
  
-    # def test_patch_img(self):
-    #     """
-    #     Testing when input file is not an image
-    #     """
-    #     class UnidentifiedImageError(Exception):
-    #         pass
-    #     try:
-    #         [ch_score, db_score] = tilseg.cluster.clustering_score(model=model_KMeans, patch_path=test_patch_path)
-    #         self.assertTrue(False)
-    #     except UnidentifiedImageError:
-    #         self.assertTrue(True)
+    def test_patch_img(self):
+        """
+        Testing when input file is not an image
+        """
+        with self.assertRaises(UnidentifiedImageError):
+            [ch_score, db_score] = tilseg.cluster.clustering_score(model=model_KMeans, patch_path=fail_test_patch_path)
 
-    # def test_model_in1(self):
-    #     """
-    #     Testing when input model is not an estimator
-    #     """
-    #     try:
-    #         [ch_score, db_score] = tilseg.cluster.clustering_score(model=5, patch_path=test_patch_path)
-    #         self.assertTrue(False)
-    #     except TypeError:
-    #         self.assertTrue(True)
+    def test_model_in1(self):
+        """
+        Testing when input model is not an estimator
+        """
+        with self.assertRaises(TypeError):
+            [ch_score, db_score] = tilseg.cluster.clustering_score(model=5, patch_path=test_patch_path)
 
-    # def test_model_in2(self):
-    #     """
-    #     Testing when input model is not fitted
-    #     """
-    #     try:
-    #         [ch_score, db_score] = tilseg.cluster.clustering_score(model=model_KMeans, patch_path=test_patch_path)
-    #         self.assertTrue(False)
-    #     except:
-    #         self.assertTrue(True)
+    def test_model_in2(self):
+        """
+        Testing when input model is not fitted
+        """
+        with self.assertRaises(NotFittedError):
+            [ch_score, db_score] = tilseg.cluster.clustering_score(model=model_unfitted, patch_path=test_patch_path)
 
     def test_cluster_model(self):
         """
         Testing when input model is not sklearn.cluster.model
         """
-        try:
+        with self.assertRaises(ValueError):
             [ch_score, db_score] = tilseg.cluster.clustering_score(model=model_linear, patch_path=test_patch_path)
-            self.assertTrue(False)
-        except ValueError:
-            self.assertTrue(True)
