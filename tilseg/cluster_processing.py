58a0255ad686d8dc4ac19cbe9a64d80509d60e1f
Ryan Brady <bradyr18@uw.edu>
2023-03-08 16:58:52 -0800

    Additional comments and changes to improve speed of post-processing. Overall minimized processing time to 30 seconds.

--- tilseg/cluster_processing.py
+++ tilseg/cluster_processing.py
@@ -35,25 +35,29 @@ def image_series_exceptions(image_array: np.ndarray, rgb_bool: bool = True):
         is the image being passed in color or grayscale
     """
 
+    # two sets of tests based on if image is color or grayscale
     if rgb_bool:
+        # checks the number of dimensions in the image array to ensure its 4
         if image_array.ndim != 4:
             raise ValueError(f"RGB images should has 4 dimensions but "
                              f"{image_array.ndim} were input")
         else:
             pass
 
+        # checks the fourth dimension to ensure it has 3 color channels RGB
         image_array_shape = image_array.shape
         if image_array_shape[3] != 3:
             raise ValueError("Image should have 3 channels for RGB")
         else:
             pass
     else:
+        # checks the number of dimensions in the image array to ensure its 3
         if image_array.ndim != 3:
             raise ValueError(f"Grayscale images should has 3 dimensions but "
                              f"{image_array.ndim} were input")
         else:
             pass
-
+        # checks pixel values to ensure they are between 0 and 255
         if image_array.max() > 255 or image_array.min() < 0:
             raise ValueError("Grayscale images should have pixel values"
                              "between 0 and 255")
@@ -80,17 +84,21 @@ def generate_image_series(image_array: np.ndarray, filepath: str,
     rgb_bool: bool
         is the image being passed in color or grayscale
     """
-
+    # call the function that generates all exceptions for this function
     image_series_exceptions(image_array, rgb_bool)
 
+    # find dimensions of array as first dimension is number of images
     dims = image_array.shape
+    # define filepath of new directory and create if it does not exist
     path = os.path.join(filepath, prefix)
     if not os.path.exists(path):
         os.mkdir(path)
     else:
         pass
+    # go to created directory
     os.chdir(path)
 
+    # save dims[0] images in folder from image_array using imsave function
     if rgb_bool:
         for count in range(dims[0]):
             plt.imsave(f"Image{count + 1}.jpg", image_array[count][:][:][:])
@@ -130,13 +138,22 @@ def gen_base_arrays(ori_image: np.ndarray, num_clusts: int):
         3 dimensional array where the dimensions correspond to X, Y, color.
         This will be used to generate an image with all clusters shown.
     """
+    # get image dimensions
     dims = ori_image.shape
-    four_dim_array = np.expand_dims(ori_image, 0)
+
+    # make an empty array as the basis for masks
     binary_array = np.zeros((num_clusts, dims[0], dims[1]))
+    # make an empty array as the basis for the all cluster image
     all_mask_array = np.zeros((dims[0], dims[1], 3), np.uint8)
+
+    #  image into 4 dimensions for appending into multiple layers
+    four_dim_array = np.expand_dims(ori_image, 0)
+    # initalize final_array and append until the first dimension has the
+    # same length as the number of clusters
     final_array = four_dim_array
     for _ in range(num_clusts - 1):
         final_array = np.vstack((final_array, four_dim_array))
+    # return all generated arrays
     return final_array, binary_array, all_mask_array
 
 
@@ -173,7 +190,7 @@ def result_image_generator(img_clust: np.ndarray, original_image: np.ndarray):
         The pixels in the array have various colors associated for each
         cluster.
     """
-
+    # generate errors if the clusters or image are the wrong dimensions
     if img_clust.ndim != 2:
         raise ValueError(f"Cluster array has 2 dimensions but "
                          f"{img_clust.ndim} were input")
@@ -186,26 +203,33 @@ def result_image_generator(img_clust: np.ndarray, original_image: np.ndarray):
     else:
         pass
 
-    # Colors that will become associated with each cluster on overlays
+    # colors that will become associated with each cluster on overlays
     black = np.array([0, 0, 0], np.uint8)
 
+    # array that holds colors used for generation of all cluster overlay
     colors_overlay = np.array(([0, 0, 0], [255, 0, 0],
                               [0, 255, 0], [0, 0, 255]), np.uint8)
 
-    # Making a dictionary of the original images that will be overwriten
+    # making a dictionary of the original images that will be overwriten
     dims = img_clust.shape
     num_clust = int(img_clust.max() + 1)
 
+    # call function that generates arrays for reassignment
     final_arrays, binary_arrays, all_masks = gen_base_arrays(original_image,
                                                              num_clust)
 
+    # itterate over every pixel in the image
     for j in range(dims[0]):
         for k in range(dims[1]):
+            # get the cluster and assign it to key
             key = int(img_clust[j][k])
+            # recolor the pixel in the overlaid image layer corresponding
+            # to the specified cluster
             final_arrays[key][j][k] = black
+            # reassign pixel in mask layer based on relevant cluster
             binary_arrays[key][j][k] = 1
-            for count in range(3):
-                all_masks[j][k][count] = colors_overlay[key][count]
+            # recolor every pixel with the cluster based on the cluster
+            all_masks[j][k] = colors_overlay[key]
 
     return final_arrays, binary_arrays, all_masks
 
@@ -227,9 +251,14 @@ def filter_boolean(contour: np.ndarray):
         otherwise
     """
 
+    # initalize boolean as false
     meets_crit = False
+    # get characteristics of contour
     perimeter = cv.arcLength(contour, True)
     area = cv.contourArea(contour)
+
+    # filter the contours containing more than one pixel based on area and
+    # roundness
     if area != 0 and perimeter != 0:
         roundness = perimeter**2 / (4 * np.pi * area)
         meets_crit = all([area > 200, area < 2000,
@@ -259,19 +288,23 @@ def contour_generator(img_mask: np.ndarray):
     contours_count: int
         number of contours that met the determined filters
     """
-
+    # raise an error if the image mask is not binary
     if img_mask.max() > 1 or img_mask.min() < 0:
         raise ValueError("Mask should only have values of 0 or 1")
     else:
         pass
 
+    # generate contours using OpenCV
     contours, _ = cv.findContours(img_mask.astype(np.int32),
                                   cv.RETR_FLOODFILL,
                                   cv.CHAIN_APPROX_NONE)
+    # generate an empty list to store filtered contours
     contours_mod = []
+    # for each contour check if it meets the filters and add to filtered list
     for ele in enumerate(contours):
         if filter_boolean(contours[ele[0]]):
             contours_mod.append(contours[ele[0]])
+    # get the number of contours after filtering
     contours_count = len(contours_mod)
     return contours_mod, contours_count
 
@@ -288,26 +321,28 @@ def csv_results_compiler(cont_list: list, filepath: str):
     filepath: str
         the filepath where the CSV file will be saved
     """
-
+    # make an empty array to hold contour parameters
     data_sum = np.zeros((len(cont_list), 4))
+    # for each contour calculate area, perimeter and other derived parameters
     for ele in enumerate(cont_list):
         temp_area = cv.contourArea(cont_list[ele[0]])
         temp_per = cv.arcLength(cont_list[ele[0]], True)
         _, temp_radius = cv.minEnclosingCircle(cont_list[ele[0]])
         temp_roundness = temp_per**2 / (4 * np.pi * temp_area)
         temp_circle_area = np.pi * temp_radius**2
+        # add contour parameters to the array
         data_sum[ele[0]][:] = [temp_area, temp_per, temp_roundness,
                                temp_circle_area]
+    # turn array into dataframe to save as CSV
     dataframe = pd.DataFrame(data_sum, columns=["Area", "Perimeter",
                                                 "Roundness",
                                                 "Bounding Circle Area"])
-
+    # define path and save CSV
     path = os.path.join(filepath, "Compiled_Data.csv")
     dataframe.to_csv(path, index=False)
 
 
-def immune_cluster_analyzer(masks: list, original_image: np.ndarray,
-                            filepath: str):
+def immune_cluster_analyzer(masks: list):
     """
     This function will generate the contours, identify the relevant cluster
     that contains the immune cells and export the data as a CSV. It will also
@@ -326,22 +361,58 @@ def immune_cluster_analyzer(masks: list, original_image: np.ndarray,
     max_contour_count
 
     """
-
+    # intialize lists to store contours and number of contours
     contour_list = []
     count_list = []
+    # initialize variables which will define the cluster which has the most
+    # contours
     count_index = 0
     max_contour_count = 0
     for ele in enumerate(masks):
+        # generate contours based on for each mask on a list
         contour_temp, contour_count = contour_generator(masks[ele[0]])
+        # append contours and countour count to lists
         contour_list.append(contour_temp)
         count_list.append(contour_count)
+        # check if the amount of contours is the largest seen and if so
+        # redefine max index and value
         if contour_count > count_list[count_index]:
             count_index = ele[0]
             max_contour_count = contour_count
         else:
             pass
-    TIL_contour = contour_list[count_index]
-    return TIL_contour, max_contour_count
+    # only get contours from the cluster with the most contours and return
+    til_contour = contour_list[count_index]
+    return til_contour, max_contour_count
+
+
+def draw_til_images(img: np.ndarray, contours: list, filepath: str):
+    """
+    This function will generate the an overlaid and mask image from the
+    contours.
+
+    Parameters
+    -----
+    img: nd.ndarray
+        3 dimensional array containing X, Y, and color data of the image that
+        will be overlaid
+    contours: list
+        list of arrays of points defining the contours that will be overlaid
+        on the images
+    filepath:
+        directory where the images will be saved
+    """
+    # get image shape and use to make an empty array
+    dims = img.shape
+    tils_mask = np.zeros((dims[0], dims[1], 3), np.uint8)
+    # draw contours on original image in green and the blank image in white
+    cv.drawContours(tils_mask, contours, -1, (255, 255, 255), 3)
+    cv.drawContours(img, contours, -1, (0, 255, 0), 3)
+    # generate relevant file paths and save overlaid image and mask
+    contour_img_filepath = os.path.join(filepath, "ContourOverlay.jpg")
+    contour_mask_filepath = os.path.join(filepath, "ContourMask.jpg")
+    plt.imsave(contour_img_filepath, img)
+    plt.imsave(contour_mask_filepath, tils_mask)
 
 
 def image_postprocessing(clusters: np.ndarray, ori_img: np.ndarray,
@@ -373,81 +444,67 @@ def image_postprocessing(clusters: np.ndarray, ori_img: np.ndarray,
     gen_csv: bool
         determines if CSV of contours will be generated
     """
-
+    # generate errors if cluster and image have incorrect dimensions
     if clusters.ndim != 2:
         raise ValueError(f"Cluster array has 2 dimensions but "
                          f"{clusters.ndim} were input")
-    else:
-        pass
 
     if ori_img.ndim != 3:
         raise ValueError(f"Original image has 3 dimensions but "
                          f"{ori_img.ndim} were input")
 
+    # generate error if the image does not have RGB channels
     ori_shape = ori_img.shape
     if ori_shape[2] != 3:
         raise ValueError("Images should have 3 channels for RGB")
-    else:
-        pass
-   
+
+    # get time to track function run time
     intial_time = time.time()
+
+    # generate masked images, masks and all cluster image via previously
+    # defined function
     masked_images, masks, all_masks = result_image_generator(clusters,
                                                              ori_img)
 
+    # modify filepath and then make directory with predefined name
     mod_filepath = os.path.join(filepath, "Clustering Results")
-
     if not os.path.exists(mod_filepath):
         os.mkdir(mod_filepath)
-    else:
-        pass
-    
+
+    # if any image generation is required, generate original image
     if any([gen_overlays, gen_masks, gen_tils, gen_all_clusters]):
         ori_filepath = os.path.join(mod_filepath, "Original.jpg")
         plt.imsave(ori_filepath, ori_img)
-    else: 
-        pass
-    
-    if gen_tils or gen_csv:
-        TIL_list, TIL_count = immune_cluster_analyzer(masks, ori_img, mod_filepath)
-    else:
-        pass
 
+    # generate contours if images or CSV of TILs is required
+    if gen_tils or gen_csv:
+        til_list, til_count = immune_cluster_analyzer(masks)
+
+    # save image with all clusters if specified
     if gen_all_clusters:
         all_clust_filepath = os.path.join(mod_filepath, "AllClusters.jpg")
         plt.imsave(all_clust_filepath, all_masks)
-    else:
-        pass
 
+    # save overlays if specifed
     if gen_overlays:
         generate_image_series(masked_images, mod_filepath, "Overlaid Images",
                               True)
-    else:
-        pass
 
+    # save TILs images if specifed
     if gen_tils:
-        dims = ori_img.shape
-        tils_mask = np.zeros((dims[0], dims[1], 3), np.uint8)        
-        cv.drawContours(tils_mask, TIL_list, -1, (255, 255, 255), 3)
-        contour_mask_filepath = os.path.join(mod_filepath, "ContourMask.jpg")
-        plt.imsave(contour_mask_filepath, tils_mask)
-        cv.drawContours(ori_img, TIL_list, -1, (0, 255, 0), 3)
-        contour_img_filepath = os.path.join(mod_filepath, "ContourOverlay.jpg")
-        plt.imsave(contour_img_filepath, ori_img)
-    else: 
-        pass
+        draw_til_images(ori_img, til_list, mod_filepath)
 
-
+    # save masks if specified
     if gen_masks:
+        # convert from binary to grayscale and save image series
         masks_imgs = masks * 255
         generate_image_series(masks_imgs, mod_filepath, "Masks", False)
-    else:
-        pass
 
+    # generate CSV and save if specified
     if gen_csv:
-        csv_results_compiler(TIL_list, mod_filepath)
-    else:
-        pass
+        csv_results_compiler(til_list, mod_filepath)
 
+    # print time taken to process
     print(f"Time to process image: {time.time()-intial_time:.3f}")
 
-    return TIL_count
+    return til_count
