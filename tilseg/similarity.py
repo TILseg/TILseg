8c75da9ac079bccc49e5be6957ab3320e441a88f
Stanley Cho <scho1@uw.edu>
2024-03-07 12:15:57 -0800

    updated mean squared error function

--- tilseg/similarity.py
+++ tilseg/similarity.py
@@ -1,38 +1,28 @@
 import numpy as np
-from PIL import Image
+import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 
-def image_similarity(array1, array2):
+def image_similarity(mask1, mask2):
     """
     calculates mean squared error and between two arrays 
     and generates the image difference.
 
     Parameters
     ----
-    array1 (np.ndarray): array of first image.
-    array2 (np.ndarray): array of second image.
+    mask1 (np.ndarray): array of first image
+    mask2 (np.ndarray): array of second image
 
     Returns:
     mse (float): mean squared error
     diff (np.ndarray): image difference as numpy array.
     """
 
-   # calculate mse
-    mse = mean_squared_error(array1, array2)
+    # calculate mse
+    mse = mean_squared_error(mask1.flatten(), mask2.flatten())
 
-    # Compute mean squared error
-    mse = np.mean(squared_diff)    
+    # calculate absolute difference 
+    abs_diff = np.abs(mask1 - mask2)
+    # # threshold the result, wrapped around 255s replaced by 1s
+    diff = np.where(abs_diff > 1, 1, abs_diff)
 
-    return mse, diff
-
-# usage:
-if __name__ == "__main__":
-    img1_path = "image1.jpg"
-    img2_path = "image2.jpg"
-
-    mse, ssim_score, diff = image_similarity(img1_path, img2_path)
-    print ("MSE:", mse)
-    print("SSIM Similarity Score:", ssim_score)
-
-    # save or display difference image
-    Image.fromarray(diff).save("difference_image.jpg")
+    return mse, diff  
