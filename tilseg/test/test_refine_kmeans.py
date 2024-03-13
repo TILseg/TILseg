91816bf56819a371ad37833b499966fd62811c6f
lisalcarr <lisalecarr@gmail.com>
2024-03-12 20:24:27 -0700

    adjested dummy inputs into one shot tests

--- tilseg/test/test_refine_kmeans.py
+++ tilseg/test/test_refine_kmeans.py
@@ -2,20 +2,40 @@
 
 # Core library imports
 import os
-import pathlib
+# import pathlib
 
 # External library imports
 import numpy as np
-import pandas as pd
-import sklearn.cluster
-from sklearn.preprocessing import StandardScaler
-from skimage.measure import label, regionprops
-import matplotlib.pyplot as plt
+# import pandas as pd
+# import sklearn.cluster
+# from sklearn.preprocessing import StandardScaler
+# from skimage.measure import label, regionprops
+# import matplotlib.pyplot as plt
 import unittest
+import sklearn
 
 # Internal imports
+from .. import refine_kmeans
 import tilseg
-import tilseg.refine_kmeans
+import tilseg.seg
+from tilseg.refine_kmeans import mask_to_features, km_dbscan_wrapper, kmean_to_spatial_model_patch_wrapper, kmean_to_spatial_model_superpatch_wrapper, KMeans_superpatch_fit
+
+
+TEST_PATCH_PATH = os.path.join(os.path.dirname(tilseg.__file__), 'test',
+                               'test_patches', 'patches',
+                               'test_small_patch.tif')
+FAIL_TEST_PATCH_PATH = os.path.join(os.path.dirname(tilseg.__file__), 'test',
+                                    'test_patches', 'test_img.txt')
+TEST_IN_DIR_PATH = os.path.join(os.path.dirname(tilseg.__file__), 'test',
+                                'test_patches', 'patches')
+TEST_OUT_DIR_PATH = os.path.join(os.path.dirname(tilseg.__file__), 'test',
+                                'test_patches', 'results')
+SUPERPATCH_PATH = os.path.join(os.path.dirname(tilseg.__file__), 'test',
+                               'test_patches', 'test_superpatch.tif')
+TEST_SPATIAL_HYPERPARAMETERS = {
+    'eps': 'value1',
+    'min_samples': 'value2',
+}
 
 class TestRefineKMeans(unittest.TestCase):
     
@@ -52,11 +72,11 @@ class TestRefineKMeans(unittest.TestCase):
         hyperparameter_dict = {'eps': 0.5, 'min_samples': 5}  # What hyperparameters are we choosing?
         save_filepath = 'path to the saved file'  # Not quite sure how to set this up for reproducability?
         
-        #Calling km_dbscan_wrapper function
+        # Calling km_dbscan_wrapper function
         all_labels, dbscan = km_dbscan_wrapper(binary_mask, hyperparameter_dict, save_filepath)
 
         # Testing the output shape
-        self.assertEqual(all_labels.shape, mask.shape)
+        self.assertEqual(all_labels.shape, binary_mask.shape)
 
         #Testing the model type
         self.assertIsInstance(dbscan, sklearn.cluster.DBSCAN)
@@ -65,16 +85,22 @@ class TestRefineKMeans(unittest.TestCase):
         # Testing al_labels output
         self.assertTrue(np.all(all_labels >= -1))  # Making sure labels are greater than -1
         self.assertTrue(all_labels.dtype == int) #Checking label types are integers
-
-class TestKMeanDBSCANImplementation(unittest.TestCase):
     
     def test_kmean_to_spatial_model_superpatch_wrapper(self):
         """
         Unittests for kmean_to_spatial_model_superpatch_wrapper function
         """
         # one-shot test with correct inputs
-        IM_labels, dbscan_fit, cluster_mask_dict = tilseg.seg.kmean_to_spatial_model_superpatch_wrapper(
-            super_patch = SUPERPATCH_PATH, in_dir_path = TEST_IN_DIR_PATH, spatial_hyperparameters={'eps': 15, 'min_samples': 200}, n_clusters=[4], out_dir_path=None)
+        IM_labels, dbscan_fit, cluster_mask_dict = kmean_to_spatial_model_superpatch_wrapper(SUPERPATCH_PATH,
+                                            TEST_IN_DIR_PATH,
+                                            TEST_SPATIAL_HYPERPARAMETERS,
+                                            TEST_OUT_DIR_PATH,
+                                            save_TILs_overlay = False,
+                                            save_cluster_masks = False,
+                                            save_cluster_overlays = False,
+                                            save_all_clusters_img = False,
+                                            save_csv = False,
+                                            random_state = None)
         
         #checks if each output type is correct
         self.assertIsInstance(IM_labels, np.ndarray)
@@ -83,12 +109,20 @@ class TestKMeanDBSCANImplementation(unittest.TestCase):
         # checks that the model outputted above is fitted
         self.assertTrue(sklearn.utils.validation.check_is_fitted(dbscan_fit) is None)
 
-    def test_kmean_dbscan_patch_wrapper(self):
+    def test_kmean_to_spatial_model_patch_wrapper(self):
         """
         Unittests for kmean_dbscan_patch_wrapper function
         """
         # one-shot test with correct inputs
-        IM_labels, dbscan_fit, cluster_mask_dict = tilseg.seg.kmean_dbscan_patch_wrapper(TEST_PATCH_PATH, spatial_hyperparameters={'eps': 15, 'min_samples': 200}, n_clusters[4], out_dir_path=None)
+        IM_labels, dbscan_fit, cluster_mask_dict = kmean_to_spatial_model_patch_wrapper(TEST_PATCH_PATH,
+                        TEST_SPATIAL_HYPERPARAMETERS,
+                        out_dir_path = TEST_OUT_DIR_PATH,
+                        save_TILs_overlay = False,
+                        save_cluster_masks = False,
+                        save_cluster_overlays = False,
+                        save_all_clusters_img = False,
+                        save_csv = False,
+                        random_state = None)
 
         #checks if each output type is correct
         self.assertIsInstance(IM_labels, np.ndarray)
